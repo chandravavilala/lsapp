302ed6f30d577989d76d29099ae42d0db6c9a71c
chandravavilala <chandra.vavilala@gmail.com>
2018-10-20 18:01:44 -0500

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome to about page</h1>
+<p>This the laravel application explaints about</p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{{$title}}</h1>
+<p>This the laravel application form the laravel from scratch</p>
+@endsection
